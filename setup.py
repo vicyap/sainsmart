8033cb11452320f4db7a67a294e09195dfbd5808
Victor Yap <victor@vicyap.com>
2017-06-14 10:14:53 -0500

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='sainsmart',
-    version='0.1.2',
+    version='0.2.0',
     description="sainsmart contains code for working with sainsmart products.",
     long_description=readme + '\n\n' + history,
     author="Victor Yap",
