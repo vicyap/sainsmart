29bd597740fe8c30c6bf816cda63265d5da71084
Victor Yap <victor@vicyap.com>
2017-06-14 11:07:37 -0500

    add typing for python 2

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'requests==2.17.3'
+    'requests==2.17.3',
+    'typing==3.6.1'
 ]
 
 setup_requirements = []
